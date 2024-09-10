b74196dd2467b7c9df5797fbb0cb9ff53c1b5c0e
junssekut <arjuna.andio@binus.ac.id>
2024-09-10 11:12:49 +0700

    Fixed some bugs.

--- assets/js/test.js
+++ assets/js/test.js
@@ -98,9 +98,9 @@ function OpenCVReady(){
             cocoSsd.load().then((loadedModel) => {
                 model = loadedModel
                 console.log("Model Loaded")
-                enableCam();
-                streaming = true;
-                recognition.start();
+                // enableCam();
+                // streaming = true;
+                // recognition.start();
             })
         }, 0);
 
