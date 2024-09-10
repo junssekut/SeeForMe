ca3b781f5a98f542b3bafa6f64aa3e241f14d5c4
junssekut <arjuna.andio@binus.ac.id>
2024-09-10 11:05:08 +0700

    Fixed website particle-js not working sometimes if the path website is only /

--- assets/js/index.js
+++ assets/js/index.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('keydown', function(event) {
         if (event.code === 'Space') {
             
-            if (window.location.pathname.includes('index.html')) {
+            if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
                 
                 window.location.href = 'seeforme.html';
             } else if (window.location.pathname.includes('seeforme.html')) {
@@ -92,7 +92,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    if (window.location.pathname.includes('index.html')) {
+    if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
+        console.log('test');
         particlesJS.load('particles-js', './assets/js/particlesjs-config.json', function() {
             console.log('callback - particles.js config loaded');
         });
