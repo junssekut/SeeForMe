5620f073ea5ffb4e0f7939ef419fb875baa4bf4e
junssekut <arjuna.andio@binus.ac.id>
2024-09-18 15:32:38 +0700

    Some fixes.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,125 +1,101 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (window.location.pathname.endsWith('index.html')) {
-        new Notify ({
-            status: 'info',
-            title: '',
-            text: 'Press SPACE to Start See For Me.',
-            effect: 'fade',
-            speed: 500,
-            customClass: '',
-            customIcon: '',
-            showIcon: true,
-            showCloseButton: false,
-            autoclose: true,
-            autotimeout: 3000,
-            notificationsGap: null,
-            notificationsPadding: null,
-            type: 'outline',
-            position: 'left top',
-            customWrapper: '',
-        });
-    }
-        
-    const scrollDownElement = document.getElementById('scroll-down');
-    if (scrollDownElement) {
-        scrollDownElement.addEventListener('click', function () {
-            console.log(scrollDownElement.style.rotate);
-            if (scrollDownElement.style.rotate === '') {
-                document.getElementById('content-1').scrollIntoView({ behavior: 'smooth' });
-            } else {
-                document.getElementById('content-base').scrollIntoView({ behavior: 'smooth' });
+const WINDOW_PATH = window.location.pathname;
+const IS_MAIN_PAGE = WINDOW_PATH.includes('index.html') || WINDOW_PATH === '' || WINDOW_PATH === '/';
+const IS_MOBILE = window.innerWidth <= 768;
+
+function init() {
+    console.log('test');
+    new Notify ({
+        status: 'info',
+        title: '',
+        text: `${IS_MAIN_PAGE ? 'Press SPACE' : 'Swipe Left'} to Start See For Me!`,
+        effect: 'fade',
+        speed: 500,
+        customClass: '',
+        customIcon: '',
+        showIcon: true,
+        showCloseButton: false,
+        autoclose: true,
+        autotimeout: 3000,
+        notificationsGap: null,
+        notificationsPadding: null,
+        type: 'outline',
+        position: 'left top',
+        customWrapper: '',
+    });
+
+    if ($('#scroll-down')) {
+        $('#scroll-down').click(function() {
+            if ($('#scroll-down').style.rotate === '') {
+                $('#content-1').scrollIntoView({ behavior: 'smooth' });
+                return;
             }
+
+            $('#content-base').scrollIntoView({ behavior: 'smooth' });
         });
     }
 
-    if (window.innerWidth <= 768) { 
+    if (IS_MOBILE) {
         let touchStartX = 0;
         let touchEndX = 0;
 
-        function handleGesture() {
-            if (touchEndX < touchStartX - 50) {
-                if (window.location.pathname.endsWith('index.html')) {
-                    window.location.href = 'seeforme.html'; 
-                }
-            }
-
-            if (touchEndX > touchStartX + 50) {
-                if (window.location.pathname.endsWith('seeforme.html')) {
-                    window.location.href = 'index.html'; 
-                }
-            }
-        }
-
-        document.addEventListener('touchstart', function(event) {
+        $(document).on('touchstart', function(event) {
             touchStartX = event.changedTouches[0].screenX;
         });
 
-        document.addEventListener('touchend', function(event) {
+        $(document).on('touchend', function(event) {
             touchEndX = event.changedTouches[0].screenX;
-            handleGesture();
+
+            if (touchEndX < touchStartX - 50 && IS_MAIN_PAGE) window.location.href = 'seeforme.html'; 
+            if (touchEndX > touchStartX + 50 && !IS_MAIN_PAGE) window.location.href = 'index.html';
         });
     }
 
-    window.addEventListener('scroll', function() {
+    $(document).on('scroll', function() {
         const scrollTo = '130px';
+        
         if ((window.innerHeight + window.scrollY + 10) >= document.documentElement.scrollHeight) {
-            if (document.getElementById('floating-button')) document.getElementById('floating-button').style.bottom = scrollTo;
-            if (this.document.getElementById('scroll-down')) document.getElementById('scroll-down').style.rotate = '-180deg';
-        } else {
-            if (document.getElementById('floating-button').style.bottom === scrollTo) {
-                document.getElementById('floating-button').style.bottom = '40px'
-            }
-
-            if (this.document.getElementById('scroll-down')) this.document.getElementById('scroll-down').style.rotate = '';
+            if ($('#floating-button')) $('#floating-button').css('bottom', scrollTo);
+            if ($('#scroll-down')) $('#scroll-down').css('rotate', '-180deg');
+            return;
         }
+
+        if ($('#floating-button').css('bottom') === scrollTo) $('#floating-button').css('bottom', '40px');
+        if ($('#scroll-down')) $('#scroll-down').css('rotate', '');
     });
 
-    document.querySelectorAll('.information-button').forEach((element) => {
-        element.addEventListener('click', () => {
-            var link = '';
-            switch (element.getAttribute('id')) {
-                case 'youtube':
-                    link = 'https://youtube.com/';
-                    break;
-                case 'phone':
-                    link = 'tel:+628333999666';
-                    break;
-                case 'instagram':
-                    link = 'https://instagram.com/';
-                    break;
-                case 'email':
-                    link = 'mailto:seeforme@gmail.com';
-                    break;
-                case 'x':
-                    link = 'https://x.com/';
-                    break;
-                default:
-                    break;
-            }
-            window.open(link, '_blank');
-        });
-    });
-
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'Space') {
-            if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
-                window.location.href = 'seeforme.html';
-            } else if (window.location.pathname.includes('seeforme.html')) {
-                
-                window.location.href = 'index.html';
-            }
+    $('.information-button').click(function(element) {
+        var link = '';
+        switch (element.id) {
+            case 'youtube':
+                link = 'https://youtube.com/';
+                break;
+            case 'phone':
+                link = 'tel:+628333999666';
+                break;
+            case 'instagram':
+                link = 'https://instagram.com/';
+                break;
+            case 'email':
+                link = 'mailto:seeforme@gmail.com';
+                break;
+            case 'x':
+                link = 'https://x.com/';
+                break;
+            default:
+                break;
         }
+        window.open(link, '_blank');
     });
 
-    if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
-        particlesJS.load('particles-js', './assets/js/particlesjs-config.json', function() {
-            console.log('callback - particles.js config loaded');
-        });
-    }
-    if (window.location.pathname.includes('seeforme.html')) {
-        particlesJS.load('particles-js', './assets/js/particlesjs-config-bubble.json', function() {
-            console.log('callback - particles.js config loaded');
-        });
-    }
-});
+    $(document).keydown(function(event) {
+        if (event.code !== 'Space') return;
+        
+        if (IS_MAIN_PAGE) window.location.href = 'seeforme.html';
+        else window.location.href = 'index.html';
+    });
 
+    if (IS_MAIN_PAGE) particlesJS.load('particles-js', './assets/js/particlesjs-config.json');
+    else particlesJS.load('particles-js', './assets/js/particlesjs-config-bubble.json');
+}
+
+$(document).ready(() => init());
